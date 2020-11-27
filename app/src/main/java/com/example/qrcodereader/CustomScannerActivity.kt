f43866fff8d0dd5e3acb3bf7a4d773dcbbae929b
Hidenori Yabuuchi <hidenori111yellow@icloud.com>
2020-11-27 13:36:42 +0900

    zxing model

---
+++ app/src/main/java/com/example/qrcodereader/CustomScannerActivity.kt
@@ -0,0 +1,45 @@
+package com.example.qrcodereader
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.journeyapps.barcodescanner.CaptureManager
+import kotlinx.android.synthetic.main.activity_custom_scanner.*
+import kotlinx.android.synthetic.main.custom_qr_code_scanner.*
+
+class CustomScannerActivity : AppCompatActivity() {
+    private lateinit var capture: CaptureManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_scanner)
+
+        capture = CaptureManager(this, barcodeView).apply {
+            initializeFromIntent(intent, savedInstanceState)
+        }
+        capture.decode()
+
+        flashSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                barcodeView.setTorchOn()
+            } else {
+                barcodeView.setTorchOff()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        capture.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        capture.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        capture.onDestroy()
+    }
+}
--- app/src/main/java/com/example/qrcodereader/MainActivity.kt
+++ app/src/main/java/com/example/qrcodereader/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.qrcodereader
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val qrButton: Button = findViewById(R.id.qr_button)
+
+        qrButton.setOnClickListener {
+            IntentIntegrator(this).apply {
+                setOrientationLocked(false)
+                setBeepEnabled(false)
+                setTorchEnabled(false)
+            }.initiateScan()
+        }
+    }
+
+    // 読取後に呼ばれる
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+
+        if (result != null) {
+            // 読取結果はresult.contentsで参照できる
+            Log.w("result.contents", result.contents)
+            Toast.makeText(this, result.contents, Toast.LENGTH_LONG).show()
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
